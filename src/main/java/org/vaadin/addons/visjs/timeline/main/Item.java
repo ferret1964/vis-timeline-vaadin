2c7a387797051efc0df3bb842c2f4b97639a2050
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2023-05-09 13:26:11 -0500

    Update Item

--- src/main/java/org/vaadin/addons/visjs/timeline/main/Item.java
+++ src/main/java/org/vaadin/addons/visjs/timeline/main/Item.java
@@ -1,6 +1,45 @@
 package org.vaadin.addons.visjs.timeline.main;
 
 import org.vaadin.addons.visjs.timeline.options.items.Items;
+import org.vaadin.addons.visjs.timeline.options.nodes.Nodes;
 
 public class Item extends Items {
+
+    private String id;
+
+    // @JsonIgnore
+    // private transient List<Edge> edgeList = new ArrayList<>();;
+    // @JsonIgnore
+    // private transient Map<String, Edge> edgeMap = new HashMap<>();;
+
+    public Item() {};
+
+    public Item(final String id) {
+        this.id = id;
+    }
+
+    public Item(final String id, Items.Builder builder) {
+        super(builder);
+        this.id = id;
+    }
+
+    public Item(String id, String title) {
+        this.id = id;
+        this.setTitle(title);
+    }
+
+    public Item(String id, String title, final Items.Builder builder) {
+        super(builder);
+        this.id = id;
+        this.setTitle(title);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
 }
